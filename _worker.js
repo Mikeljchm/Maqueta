9300dcb86b1997afce6f5e76f1109c539443324f
Mikeljchm <chuellomikel@gmail.com>
2026-06-19 22:48:42 -0400

    feat: BoyfriendTV con patron real en el detector espejo del servidor

--- _worker.js
+++ _worker.js
@@ -148,6 +148,8 @@ var EMBED_PLATFORMS = [
     embed: function(id){ return 'https://www.redgifs.com/ifr/'+id; } },
   { type: 'thisvid', match: /thisvid\.com\/(?:embed\/|videos\/[^\/]*-)?(\d+)/,
     embed: function(id){ return 'https://thisvid.com/embed/'+id+'/'; } },
+  { type: 'boyfriendtv', match: /boyfriendtv\.com\/embed\/(\d+)\/(\d+)\/(\d+)\/(\d+)/,
+    embed: function(id, m){ return 'https://www.boyfriendtv.com/embed/'+m[1]+'/'+m[2]+'/'+m[3]+'/'+m[4]+'/'; } },
   { type: 'pornhub', match: /pornhub\.com\/(?:view_video\.php\?viewkey=|embed\/)([a-zA-Z0-9]+)/,
     embed: function(id){ return 'https://www.pornhub.com/embed/'+id; } },
   { type: 'xhamster', match: /xhamster\d?\.com\/(?:videos\/[^\/]*-(\d+)|embed\/(\d+))/,
